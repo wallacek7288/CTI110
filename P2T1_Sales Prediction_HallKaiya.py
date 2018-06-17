9a8bdb7315768ec5c0e5412e4a5476462ea40b01
wallacek7288 <40342697+wallacek7288@users.noreply.github.com>
2018-06-17 15:58:58 -0400

    First Submission

---
+++ P2T1_Sales Prediction_HallKaiya.py
@@ -0,0 +1,13 @@
+#CTI-110
+#P2T1- Sales Prediction
+#Kaiya Hall
+#6.17.18
+# Get the projected total sales
+total_sales = float(input('Enter the projected sales: '))
+
+#Calculate the progit as 23 percent of total sales
+profit = total_sales * 0.23
+
+#display the profit
+print('The profit is $', format(profit, ',.2f'))
+
